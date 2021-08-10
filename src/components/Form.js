66797f43c7be673f579eaefd9bdc179dd90d4b2b
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-10 14:44:04 +0900

    useRef 사용하기

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styled from 'styled-components/native';
 import Button from './Button';
 
@@ -17,10 +17,14 @@ const StyledText = styled.Text`
 const Form = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  let count = 0;
+
+  const refName = useRef(null);
+  const refEmail = useRef(null);
 
   useEffect(() => {
     console.log('=-=-=-=-= Mount =-=-=-=-=');
+    // 처음에 Name input칸에 고정되어 키보드가 열리고
+    refName.current.focus();
     return () => console.log('=-=-=-=-= UnMount =-=-=-=-=-=');
   }, []);
 
@@ -28,14 +32,29 @@ const Form = () => {
     console.log(`name: ${name}, email: ${email}`);
   }, [email]);
 
+  const onSubmit = () => console.log('submit!');
+
   return (
     <>
       <StyledText>Name: {name}</StyledText>
       <StyledText>Email: {email}</StyledText>
-      <StyledInput value={name} onChangeText={setName} placeholder="name" />
-      <StyledInput value={email} onChangeText={setEmail} placeholder="email" />
-      {/* useEffect에 쓴 함수가 정말 리렌더링이 될 때만 실행이 되는지를 확인하기위해 관련없는 count변수를 사용해서 비교 */}
-      <Button title="count" onPress={() => count + 1} />
+      <StyledInput
+        value={name}
+        onChangeText={setName}
+        placeholder="name"
+        ref={refName}
+        returnKeyType="next"
+        // next 키를 누르면 Email input 칸으로 넘어감
+        onSubmitEditing={() => refEmail.current.focus()}
+      />
+      <StyledInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="email"
+        ref={refEmail}
+        returnKeyType="done"
+        onSubmitEditing={onSubmit}
+      />
     </>
   );
 };
